52f75be7975725bddf9c6550a8185cba4a5f87a1
srinathi31 <100667245+srinathi31@users.noreply.github.com>
2024-01-09 16:16:57 +0530

    Delete Backend directory

--- Backend/database/model/demos.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const userSchema = Schema({
-  email: String,
-  userName: String,
-});
-
-module.exports = mongoose.model("users", userSchema);
--- Backend/util/util1.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-// Return hash256 for a given message
-const hash256 = (message) => {
-  return crypto.createHash("sha256").update(message).digest("hex");
-};
-
-module.exports = { hash256 };
